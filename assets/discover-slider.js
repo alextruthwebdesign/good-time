724ecf3b7579f68b65a4791bee15fa9c6f8b4088
alex <alex@truthwebdesign.com>
2024-09-18 12:53:07 +0300

    discover section

---
+++ assets/discover-slider.js
@@ -0,0 +1,23 @@
+const discoverSliders = document?.querySelectorAll(".discover-section");
+console.log(discoverSliders);
+discoverSliders?.forEach((item) => {
+  const sliderEl = item?.querySelector(".keen-slider");
+
+  const slider = new KeenSlider(sliderEl, {
+    breakpoints: {
+      "(min-width: 430px)": {
+        slides: { perView: 1.8 },
+      },
+      "(min-width: 745px)": {
+        slides: { perView: 2.8 },
+      },
+      "(min-width: 865px)": {
+        slides: { perView: 3.3 },
+      },
+      "(min-width: 1145px)": {
+        slides: { perView: 3.85, spacing: 10 },
+      },
+    },
+    slides: { perView: 1.52 },
+  });
+});
